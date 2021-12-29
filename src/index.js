ab1f0cc3e732346a1c4fbc138641c6c3e95c7e9a
Spike Ganush <florian.jourdain@gmail.com>
2021-12-29 16:18:40 +1100

    Small changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import { Provider } from 'react-redux'
-// import { applyMiddleware, createStore } from 'redux'
 import App from './App'
-// import thunk from 'redux-thunk'
-// import rootReducer from './reducers'
-// import { getUsers } from './actions/users.actions'
 
-// dev tools
-// import { composeWithDevTools } from 'redux-devtools-extension'
-
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// )
-
-ReactDOM.render(
-  // <Provider store={store}>
-  <App />,
-  // </Provider>
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
